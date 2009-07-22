bef0f262a837a7846de0c8efb04938f187138e69
halcyon22 <halcyon22@fc8f00da-2a7f-11de-87eb-090b1b94733a>
2009-07-22 05:11:14 +0000

    start on db upgrade code

--- branches/schedules/src/com/roozen/SoundManager/utils/SQLiteDatabaseHelper.java
+++ branches/schedules/src/com/roozen/SoundManager/utils/SQLiteDatabaseHelper.java
@@ -1,9 +1,15 @@
 package com.roozen.SoundManager.utils;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.media.AudioManager;
 import android.util.Log;
+
+import com.roozen.SoundManager.R;
 
 /**
  * @author Mike Partridge
@@ -13,6 +19,7 @@ public class SQLiteDatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "data";
     private static final int DATABASE_VERSION = 3;
+    private Context mContext;
 
     /*
      * Preferences table
@@ -84,6 +91,7 @@ public class SQLiteDatabaseHelper extends SQLiteOpenHelper {
     
     public SQLiteDatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        mContext = context;
     }
 
     /* (non-Javadoc)
@@ -91,7 +99,6 @@ public class SQLiteDatabaseHelper extends SQLiteOpenHelper {
      */
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(PREFERENCES_CREATE);
         db.execSQL(SCHEDULE_TABLE_CREATE);
     }
 
@@ -104,7 +111,146 @@ public class SQLiteDatabaseHelper extends SQLiteOpenHelper {
                 "Upgrading database from version " + oldVersion + " to "
                 + newVersion + ".");
         
-        //TODO: if changing from db version 2 to 3, create schedule table, copy old timer prefs to schedules
+        if (oldVersion < 3) {
+            upgradeTo3(db);
+        }
+
+        Log.w(SQLiteDatabaseHelper.class.toString(), "Upgrade completed.");
     }
 
+    private void upgradeTo3(SQLiteDatabase db) {
+        
+        db.execSQL(SCHEDULE_TABLE_CREATE);
+        
+        //copy the system timer
+        if (getBooleanPref(db, mContext.getString(R.string.EnableSystem))) {
+            String time = getStringPref(db, mContext.getString(R.string.SystemTimeStart));
+            int volume = getIntPref(db, mContext.getString(R.string.SystemStartVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_SYSTEM);
+            
+            time = getStringPref(db, mContext.getString(R.string.SystemTimeEnd));
+            volume = getIntPref(db, mContext.getString(R.string.SystemEndVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_SYSTEM);
+        }
+        
+        //copy the ringer timer
+        if (getBooleanPref(db, mContext.getString(R.string.EnableRinger))) {
+            String time = getStringPref(db, mContext.getString(R.string.RingerTimeStart));
+            int volume = getIntPref(db, mContext.getString(R.string.RingerStartVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_RING);
+            
+            time = getStringPref(db, mContext.getString(R.string.RingerTimeEnd));
+            volume = getIntPref(db, mContext.getString(R.string.RingerEndVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_RING);
+        }
+        
+        //copy the media timer
+        if (getBooleanPref(db, mContext.getString(R.string.EnableMedia))) {
+            String time = getStringPref(db, mContext.getString(R.string.MediaTimeStart));
+            int volume = getIntPref(db, mContext.getString(R.string.MediaStartVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_MUSIC);
+            
+            time = getStringPref(db, mContext.getString(R.string.MediaTimeEnd));
+            volume = getIntPref(db, mContext.getString(R.string.MediaEndVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_MUSIC);
+        }
+        
+        //copy the alarm timer
+        if (getBooleanPref(db, mContext.getString(R.string.EnableAlarm))) {
+            String time = getStringPref(db, mContext.getString(R.string.AlarmTimeStart));
+            int volume = getIntPref(db, mContext.getString(R.string.AlarmStartVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_ALARM);
+            
+            time = getStringPref(db, mContext.getString(R.string.AlarmTimeEnd));
+            volume = getIntPref(db, mContext.getString(R.string.AlarmEndVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_ALARM);
+        }
+        
+        //copy the in-call timer
+        if (getBooleanPref(db, mContext.getString(R.string.EnableIncall))) {
+            String time = getStringPref(db, mContext.getString(R.string.IncallTimeStart));
+            int volume = getIntPref(db, mContext.getString(R.string.IncallStartVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_VOICE_CALL);
+            
+            time = getStringPref(db, mContext.getString(R.string.IncallTimeEnd));
+            volume = getIntPref(db, mContext.getString(R.string.IncallEndVolume));
+            insertSchedule(db, time, volume, AudioManager.STREAM_VOICE_CALL);
+        }
+        
+        //copy the mute pref to shared prefs
+        boolean muted = getBooleanPref(db, mContext.getString(R.string.muted));
+        if (muted) {
+            SharedPreferences prefs = mContext.getSharedPreferences(Util.PREFS_NAME, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean(mContext.getString(R.string.muted), muted);
+        }
+        
+        //TODO: cancel existing alarms
+        
+        db.execSQL("drop table "+PREFERENCE_TABLE);
+    }
+    
+    private String getStringPref(SQLiteDatabase db, String pref) {
+        String value = "";
+        
+        Cursor c = db.query(PREFERENCE_TABLE, new String[]{PREFERENCES_STRING_DATA}, 
+                            PREFERENCES_PREFERENCE+"=\""+pref+"\"", null, 
+                            null, null, null);
+        
+        if (c != null && c.moveToFirst()) {
+            value = c.getString(c.getColumnIndex(PREFERENCES_STRING_DATA));
+        }
+        
+        return value;
+    }
+    
+    private int getIntPref(SQLiteDatabase db, String pref) {
+        int value = 0;
+        
+        Cursor c = db.query(PREFERENCE_TABLE, new String[]{PREFERENCES_INTEGER_DATA}, 
+                            PREFERENCES_PREFERENCE+"=\""+pref+"\"", null, 
+                            null, null, null);
+        
+        if (c != null && c.moveToFirst()) {
+            value = c.getInt(c.getColumnIndex(PREFERENCES_INTEGER_DATA));
+        }
+        
+        return value;
+    }
+    
+    private boolean getBooleanPref(SQLiteDatabase db, String pref) {
+        boolean value = false;
+        
+        Cursor c = db.query(PREFERENCE_TABLE, new String[]{PREFERENCES_INTEGER_DATA}, 
+                            PREFERENCES_PREFERENCE+"=\""+pref+"\"", null, 
+                            null, null, null);
+        
+        if (c != null && c.moveToFirst()) {
+            value = (c.getInt(c.getColumnIndex(PREFERENCES_INTEGER_DATA)) > 0);
+        }
+        
+        return value;
+    }
+    
+    private void insertSchedule(SQLiteDatabase db, String timeStart, int volume, int stream) {
+        
+        ContentValues values = new ContentValues();
+
+        values.put(SCHEDULE_DAY0, "1");
+        values.put(SCHEDULE_DAY1, "1");
+        values.put(SCHEDULE_DAY2, "1");
+        values.put(SCHEDULE_DAY3, "1");
+        values.put(SCHEDULE_DAY4, "1");
+        values.put(SCHEDULE_DAY5, "1");
+        values.put(SCHEDULE_DAY6, "1");
+        values.put(SCHEDULE_START_HOUR, Integer.parseInt(timeStart.substring(0, timeStart.indexOf(":"))));
+        values.put(SCHEDULE_START_MINUTE, Integer.parseInt(timeStart.substring(timeStart.indexOf(":") + 1)));
+        values.put(SCHEDULE_VOLUME, volume);
+        values.put(SCHEDULE_VIBRATE, "0");
+        values.put(SCHEDULE_ACTIVE, "1");
+        values.put(SCHEDULE_TYPE, stream);
+        
+        db.insert(SCHEDULE_TABLE, null, values);
+    }
+    
 }
