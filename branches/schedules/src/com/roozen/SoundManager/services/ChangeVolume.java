bd6aefc84ae16a73234b70b70369338a8bc51f0a
halcyon22 <halcyon22@fc8f00da-2a7f-11de-87eb-090b1b94733a>
2009-07-29 05:21:03 +0000

    rewrote for schedules

--- branches/schedules/src/com/roozen/SoundManager/services/ChangeVolume.java
+++ branches/schedules/src/com/roozen/SoundManager/services/ChangeVolume.java
@@ -29,7 +29,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 
-import com.roozen.SoundManager.RingmodeToggle;
 import com.roozen.SoundManager.provider.ScheduleProvider;
 import com.roozen.SoundManager.utils.SQLiteDatabaseHelper;
 
@@ -73,7 +72,7 @@ public class ChangeVolume extends Service {
                     int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
                     
                     /*
-                     * is the schedule setup for today?
+                     * if the schedule is setup for today, apply the settings
                      */
                     if (dayOfWeek == Calendar.SUNDAY && day0 ||
                             dayOfWeek == Calendar.MONDAY && day1 ||
@@ -84,85 +83,94 @@ public class ChangeVolume extends Service {
                             dayOfWeek == Calendar.SATURDAY && day6                            
                     ) {
                         
-                        //TODO: based on volumeType, these may or may not all be called,
-                        // and the vibrate type may differ
-                        
-                        checkVolume(volumeType, volume, AudioManager.VIBRATE_TYPE_RINGER);
-                        checkRingmode(volume);
-                        checkVibrate(AudioManager.VIBRATE_TYPE_RINGER, volume);
+                        switch (volumeType) {
+                            case AudioManager.STREAM_SYSTEM:
+                                setVolume(volumeType, volume);
+                                break;
+                            case AudioManager.STREAM_RING:
+                                setRingmode(volume, vibrate);
+                                setVolume(volumeType, volume);
+                                setVibration(AudioManager.VIBRATE_TYPE_RINGER, vibrate);                                
+                                break;
+                            case AudioManager.STREAM_MUSIC:
+                                setVolume(volumeType, volume);
+                                break;
+                            case AudioManager.STREAM_ALARM:
+                                setVolume(volumeType, volume);
+                                break;
+                            case AudioManager.STREAM_VOICE_CALL:
+                                setVolume(volumeType, volume);
+                                break;
+                            case AudioManager.STREAM_NOTIFICATION:
+                                setVolume(volumeType, volume);
+                                setVibration(AudioManager.VIBRATE_TYPE_RINGER, vibrate);
+                                break;
+                        }
                                                 
-                    }
+                    } //schedule applies today
                     
                 } //end active check
                 
             } //end schedule cursor check
             
             scheduleCursor.close();		    
-		}
+		} //schedule id was found
 		
 	}
 	
-    private void checkVolume(int stream, int vol, int vibrateType) {
-        if (vol != -1) {
-            final AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+    private void setVolume(int stream, int volume) {
+        final AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-            int volMax = audio.getStreamMaxVolume(stream);
-            if (vol > volMax) {
-                vol = volMax;
-            }
-            else if (vol < 0) {
-                vol = 0;
-            }
-
-            if (vibrateType == AudioManager.VIBRATE_TYPE_RINGER) {
-                RingmodeToggle.fixRingMode(audio, vol);
-            }
-
-            int flags = AudioManager.FLAG_PLAY_SOUND;
-            flags = flags | AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE;
-            flags = flags | AudioManager.FLAG_SHOW_UI;
-            flags = flags | AudioManager.FLAG_VIBRATE;
-
-            audio.setStreamVolume(stream, vol, flags);
+        int maxVolume = audio.getStreamMaxVolume(stream);
+        if (volume > maxVolume) {
+            volume = maxVolume;
         }
+        else if (volume < 0) {
+            volume = 0;
+        }
+
+        int flags = AudioManager.FLAG_PLAY_SOUND;
+        flags = flags | AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE;
+        flags = flags | AudioManager.FLAG_SHOW_UI;
+        flags = flags | AudioManager.FLAG_VIBRATE;
+
+        /*
+         * apply volume to the system
+         */
+        audio.setStreamVolume(stream, volume, flags);
     }
 
-    private void checkRingmode(int type) {
-        if (type != -1) {
-            final AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+    private void setRingmode(int volume, boolean vibrate) {
+        final AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
-            switch (type) {
-                case RingmodeToggle.SILENT:
-                    audio.setRingerMode(AudioManager.RINGER_MODE_SILENT);
-                    break;
-                case RingmodeToggle.VIBRATE_ONLY:
-                    audio.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
-                    break;
-                case RingmodeToggle.RINGER_ONLY:
-                    audio.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-                    int vibrateSetting = audio.getVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER);
-                    if (vibrateSetting == AudioManager.VIBRATE_SETTING_ON) {
-                        audio.setVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER,
-                                                AudioManager.VIBRATE_SETTING_ONLY_SILENT);
-                    }
-                    else {
-                        audio.setVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER, AudioManager.VIBRATE_SETTING_OFF);
-                    }
-                    break;
-                case RingmodeToggle.RINGER_VIBRATE:
-                    audio.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
-                    audio.setVibrateSetting(AudioManager.VIBRATE_TYPE_RINGER, AudioManager.VIBRATE_SETTING_ON);
-                    break;
-            }
+        /*
+         * apply ringmode to the system
+         */
+        if (volume < 1 && !vibrate) {
+            audio.setRingerMode(AudioManager.RINGER_MODE_SILENT);
         }
+        else if (volume < 1 && vibrate) {
+            audio.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+        }
+        else if (volume > 0) {
+            audio.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+        }
+
     }
 
-    private void checkVibrate(int type, int vibrateSetting) {
-        if (type != -1 && vibrateSetting != -1) {
-            final AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-            audio.setVibrateSetting(type, vibrateSetting);
-            RingmodeToggle.fixRingMode(audio);
+    private void setVibration(int type, boolean vibrate) {
+        final AudioManager audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        
+        /*
+         * apply vibrate settings to the system
+         */
+        if (vibrate) {
+            audio.setVibrateSetting(type, AudioManager.VIBRATE_SETTING_ON);
         }
+        else {
+            audio.setVibrateSetting(type, AudioManager.VIBRATE_SETTING_OFF);
+        }
+        
     }
 	
 	@Override
